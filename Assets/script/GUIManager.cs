57b302813cc65a0e7d07cd2a02e653a749eb3436
Yahav-Cohen <yahav@viewerslogic.com>
2015-12-14 21:49:55 +0200

    Second commit

---
+++ Assets/script/GUIManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour {
+
+	public static GUIManager instance;
+
+	public Image bar;
+	public float max_health = 100f;
+	float cur_health = 0f;
+	
+	// Use this for initialization
+	void Start () {
+		if (instance == null)
+			instance = new GUIManager ();
+		else if (instance != this)
+			Destroy (this);
+
+		cur_health = max_health;
+		InvokeRepeating ("decreaseHealth", 0f, 2f);
+	}
+
+	public void decreaseHealth() {
+		cur_health -= 20f;
+		float calc_health = cur_health / max_health;
+		SetHealthBar (calc_health);
+	}
+
+	public void SetHealthBar(float health)
+	{
+		Debug.Log ("where here");
+
+		if (bar != null)
+			bar.fillAmount = health;
+		else
+			Debug.Log ("Image not initialized");
+	}
+}
---
+++ Assets/script/Player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private float maxHealth = 100f;
+	bool unTouchable = false;
+
+	[System.Serializable]  
+	public class PlayerStats {
+		public float health = 100f;
+	}
+
+	public static PlayerStats playerStats;
+
+
+	void Start() {
+		playerStats = new PlayerStats();
+	}
+
+	public void DamagePlayer (float damage) {
+		if (unTouchable){
+			Debug.Log("Player UnTouchable mother fucker!!!!!!!");
+			return;
+		}
+
+		playerStats.health -= damage;
+		Debug.Log(damage);
+		Debug.Log(playerStats.health);
+
+		if (playerStats.health <= 0) {
+			GameManager.KillPlayer(this);
+		}
+	}
+
+	public void RevivePlayer (float revive) {
+
+		if (playerStats.health <= maxHealth) {
+			playerStats.health += revive;
+		}
+	}
+
+	public void SetUnTouchable(bool a) {
+		unTouchable = a;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// When to damage the player. when enemy hits me - rigidbody. 
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			DamagePlayer(100);
+		}
+	}
+}
